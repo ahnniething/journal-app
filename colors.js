a53134d303019757bd5f97f28755b48843eb12ab
ahnniething <ahnniething@gmail.com>
2022-07-07 16:14:19 +0900

    #1 feat: add stack navigation and set colors

---
+++ colors.js
@@ -0,0 +1,20 @@
+const pantone = {
+  amusements: {
+    tawnyOrange: "#D67D70",
+    veryPery: "#6868AC",
+    icedCoffee: "#B4906C",
+    pinkFlambe: "#D65179",
+    fuchsiaPink: "#E187B8",
+    paradisePink: "#E9445E",
+    cornsilk: "#ECC371",
+    tourmaline: "#85A1AC",
+  },
+};
+
+const colors = {
+  bgColor: pantone.amusements.fuchsiaPink,
+  textColor: pantone.amusements.veryPery,
+  cardColor: pantone.amusements.icedCoffee,
+  btnColor: pantone.amusements.paradisePink,
+};
+export default colors;
