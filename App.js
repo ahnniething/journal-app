2d812c5b0db01cb16bdcef62b9fb6b2793539da3
ahnniething <ahnniething@gmail.com>
2022-07-11 09:28:25 +0900

    #3 feat: install realm and define an object schema

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import Navigator from "./navigator";
+import Realm from "realm";
+
+const FeelingSchema = {
+  name: "Feeling",
+  properties:{
+    _id: "int",
+    emotion: "string",
+    message: "string",
+  },
+  primaryKey: "_id"
+}
 
 export default function App() {
   return (
