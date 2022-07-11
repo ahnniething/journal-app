2ad60753970c5ffe2e367dc69b964fa56cdb4647
ahnniething <ahnniething@gmail.com>
2022-07-11 23:46:51 +0900

    #5 feat: filter realm data

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
 import { Ionicons } from "@expo/vector-icons";
-import colors from "../colors"
+import colors from "../colors";
+import { useDB } from "../context";
 
 const View = styled.View`
   flex: 1;
   padding: 30px;
   padding-top: 100px;
-  background-color: ${colors.bgColor}
+  background-color: ${colors.bgColor};
 `;
 const Title = styled.Text`
   color: ${colors.textColor};
@@ -25,16 +26,24 @@ const Btn = styled.TouchableOpacity`
   justify-content: center;
   align-items: center;
   background-color: ${colors.btnColor};
-  elevation: 5;
   box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
 `;
 
-const Home = ({ navigation: { navigate } }) => (
-  <View>
-    <Title>My journal</Title>
-    <Btn onPress={() => navigate("Write")}>
-      <Ionicons name="add" color={colors.btnTintColor} size={40} />
-    </Btn>
-  </View>
-);
+const Home = ({ navigation: { navigate } }) => {
+  const realm = useDB();
+  const [feelings, setFeelings] = useState(realm.objects("Feeling"));
+  useEffect(() => {
+    const happy = feelings.filtered("emotion = '🥰' ");
+    console.log(happy);
+  }, []);
+  return (
+    <View>
+      <Title>My journal</Title>
+      <Btn onPress={() => navigate("Write")}>
+        <Ionicons name="add" color={colors.btnTintColor} size={40} />
+      </Btn>
+    </View>
+  );
+};
+
 export default Home;
